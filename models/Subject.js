4484a54b4ecfda0a4fa027a4463f08eb1f7f3e8e
Work-249 <workplan@zohomail.in>
2025-12-06 21:40:58 +0530

    Delete models directory

--- models/Subject.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const subjectSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  topics: [{ type: String }]
-}, { timestamps: true });
-
-module.exports = mongoose.model('Subject', subjectSchema);
